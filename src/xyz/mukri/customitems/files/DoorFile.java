8bffa6e6c55ecded59fd20907cbf583213a0a1fb
MukriXYZ <50009694+MukriXYZ@users.noreply.github.com>
2019-09-08 00:23:36 +0800

    Door File updated

--- src/xyz/mukri/customitems/files/DoorFile.java
+++ src/xyz/mukri/customitems/files/DoorFile.java
@@ -1,9 +1,13 @@
 package xyz.mukri.customitems.files;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import xyz.mukri.customitems.Core;
 
@@ -25,6 +29,55 @@ public class DoorFile {
 		return config;
 	}
 	
+	public void save() {
+		try {
+			
+			config.save(file);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createNewFile() {
+		if (!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		
+		try {
+			
+			file.createNewFile();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addNewDoor(Location loc, Player owner) {
+		config.set("door." + locationToString(loc) + ".owner", owner.getName());
+	}
+	
+	public void removeDoor(Location loc) {
+		config.set("door." + locationToString(loc), null);
+	}
+	
+	public String getDoorOwner(Location loc) {
+		return config.getString("door." + locationToString(loc) + ".owner");
+	}
+	
+	public boolean isDoorOwner(Player p, Location loc) {
+		return getDoorOwner(loc).equals(p.getName());
+	}
+	
+	// Functions that we need for this class
+	public String locationToString(Location loc) {
+        return loc.getWorld().getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ();
+    }
+	
+	public Location stringToLocation(String s) {
+        String[] loc = s.split(":");
+        return new Location(Bukkit.getWorld(loc[0]), Integer.parseInt(loc[1]), Integer.parseInt(loc[2]), Integer.parseInt(loc[3]));
+    }
 	
 	
 }
